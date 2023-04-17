c0cbcfca197492139e0473cb95b0c7addff1241c
awolosewicz <112141084+awolosewicz@users.noreply.github.com>
2023-04-17 23:42:37 +0000

    Modified start_upf_controlplane.sh to avoid unneeded loop iterations

--- p4/start_upf_controlplane.sh
+++ p4/start_upf_controlplane.sh
@@ -46,46 +46,45 @@ done
 
 #handle=$(table_add "downlink NoAction 192.0.0.2 =>")
 #echo $handle
-
+q=0
 while :; do
-    for (( q=0; q<$REG_SIZE; q++ )); do
-        ctr=$(read_register "ctrs_r $q")
-        if [ $ctr != ${ctrs[$q]} ]; then
-            echo "New UL entry recorded"
-            ip_exist=0
-            ip=$(read_register "ue_ips_r $q")
-            teid=$(read_register "ue_teids_r $q")
-            swport=$(read_register "swports_r $q")
-            s1u_da=$(read_register "s1u_das_r $q")
-            s1u_sa=$(read_register "s1u_sas_r $q")
-            enb_ip=$(read_register "enb_ips_r $q")
-            s1u_ip=$(read_register "s1u_ips_r $q")
-            for r in ${!ips[@]}; do
-                if [ $ip == ${ips[$r]} ]; then
-                    ip_exist=1
-                    if [[ $teid != ${teids[$r]} || $swport != ${swports[$r]} || $s1u_da != ${s1u_das[$r]} ||\
-                          $s1u_sa != ${s1u_sas[$r]} || $enb_ip != ${enb_ips[$r]} || $s1u_ip != ${s1u_ips[$r]} ]]
-                        then
-                        echo "Updating existing DL entry for UE with IP $ip"
-                        catch=$(table_modify "downlink dlentry_add ${handles[$r]} $swport $s1u_da $s1u_sa $enb_ip $s1u_ip $teid")
-                    else
-                        echo "Nothing to update"
-                    fi
+    ctr=$(read_register "ctrs_r $q")
+    if [ $ctr != ${ctrs[$q]} ]; then
+        echo "New UL entry recorded"
+        ip_exist=0
+        ip=$(read_register "ue_ips_r $q")
+        teid=$(read_register "ue_teids_r $q")
+        swport=$(read_register "swports_r $q")
+        s1u_da=$(read_register "s1u_das_r $q")
+        s1u_sa=$(read_register "s1u_sas_r $q")
+        enb_ip=$(read_register "enb_ips_r $q")
+        s1u_ip=$(read_register "s1u_ips_r $q")
+        for r in ${!ips[@]}; do
+            if [ $ip == ${ips[$r]} ]; then
+                ip_exist=1
+                if [[ $teid != ${teids[$r]} || $swport != ${swports[$r]} || $s1u_da != ${s1u_das[$r]} ||\
+                      $s1u_sa != ${s1u_sas[$r]} || $enb_ip != ${enb_ips[$r]} || $s1u_ip != ${s1u_ips[$r]} ]]
+                    then
+                    echo "Updating existing DL entry for UE with IP $ip"
+                    catch=$(table_modify "downlink dlentry_add ${handles[$r]} $swport $s1u_da $s1u_sa $enb_ip $s1u_ip $teid")
+                else
+                    echo "Nothing to update"
                 fi
-            done
-            if [ $ip_exist == 0 ]; then
-                echo "Adding new DL entry for UE with IP $ip"
-                handle=$(table_add "downlink dlentry_add $ip => $swport $s1u_da $s1u_sa $enb_ip $s1u_ip $teid")
-                ips+=( $ip )
-                teids+=( $teid )
-                swports+=( $swport )
-                s1u_das+=( $s1u_da )
-                s1u_sas+=( $s1u_sa )
-                enb_ips+=( $enb_ip )
-                s1u_ips+=( $s1u_ip )
-                handles+=( $handle )
             fi
-            ctrs[$q]=$ctr
+        done
+        if [ $ip_exist == 0 ]; then
+            echo "Adding new DL entry for UE with IP $ip"
+            handle=$(table_add "downlink dlentry_add $ip => $swport $s1u_da $s1u_sa $enb_ip $s1u_ip $teid")
+            ips+=( $ip )
+            teids+=( $teid )
+            swports+=( $swport )
+            s1u_das+=( $s1u_da )
+            s1u_sas+=( $s1u_sa )
+            enb_ips+=( $enb_ip )
+            s1u_ips+=( $s1u_ip )
+            handles+=( $handle )
         fi
-    done
+        ctrs[$q]=$ctr
+        ((q++))
+    fi
 done
